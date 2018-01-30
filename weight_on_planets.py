87f72f88328e9a6d46253728773e15757cf4c3c0
ericmiller84143 <35577371+ericmiller84143@users.noreply.github.com>
2018-01-30 15:04:08 -0500

    Update weight_on_planets.py

--- weight_on_planets.py
+++ weight_on_planets.py
@@ -16,6 +16,8 @@ for line in infile:  #loop over each line in the code
   temp = line.split(;)  #delimiter is the ; 
   if temp[0] == name:   #if the first element of the line matches the name user wants
     radius = temp[1]/1000   #set radius to the planet radius
-    density = temp[2]       #set density to the planet density
+    density = temp[2]*1000      #set density to the planet density
     break                   #stop looping because the planet and respective data was found
-  
+ 
+
+accel = (4/3)*ma.pi
