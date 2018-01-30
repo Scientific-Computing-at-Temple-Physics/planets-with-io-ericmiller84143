8b6d8750f8b7c3992e93f6fda28f5e374f8ca5ee
ericmiller84143 <35577371+ericmiller84143@users.noreply.github.com>
2018-01-30 14:37:52 -0500

    Update weight_on_planets.py

--- weight_on_planets.py
+++ weight_on_planets.py
@@ -1,4 +1,6 @@
 # This is a comment.  Python will ignore these lines (starting with #) when running
 
 import math as ma
-# To use a math function, write "ma." in front of it.  Example:  ma.sin(3.146)
+
+
+infile = open("planet_data.dat", "r")
