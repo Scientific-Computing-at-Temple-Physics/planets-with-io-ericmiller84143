678d653d996c5b894ba001619be10fec14c50318
ericmiller84143 <35577371+ericmiller84143@users.noreply.github.com>
2018-01-30 21:47:01 -0500

    Update weight_on_planets.py

--- weight_on_planets.py
+++ weight_on_planets.py
@@ -5,8 +5,8 @@
 
 import math as ma
 
-density = 0.0
-radius =0.0
+"""density = 0.0
+radius =0.0"""
 G=6.674*(10**(-11))
 
 mass = input("What is the mass of the explorer? :")
@@ -18,13 +18,19 @@ infile = open("planet_data.dat", "r")
 for line in infile:  #loop over each line in the code
   temp = line.split(";")  #delimiter is the ;, split each line like this
   if temp[0].upper() == name.upper():   #if the first element of the line matches the name the user specified
-    radius = temp[1]*1000   #set radius to the planet radius
-    density = temp[2]*1000      #set density to the planet density
+    temp2 = temp[1].split()
+    temp3 = temp[2].split()
+    radius = float(temp2[0])*1000   #set radius to the planet radius
+    density = float(temp3[0])*1000      #set density to the planet density
+    print (radius, density)
     break                   #stop looping because the planet and respective data was found
  
 
 accel = (4/3)*(ma.pi)*G*radius*density
+print "Acceleration:", accel
+weight = accel * mass
+gs = accel / 9.8
 
-print "The explorer's weight is", accel*mass, "N. The gravitational acceleration is", accel/9.8 , "g's"
+print "The explorer's weight is", weight, "N. The gravitational acceleration is", gs , "g's"
 
 infile.close()
